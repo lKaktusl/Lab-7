6a7a3c382db9cf6ce1894be5ebf5ebc5adbe0975
lKaktusl <44606965+lKaktusl@users.noreply.github.com>
2018-12-11 13:50:32 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Products product1 = new Products("Shopcountry", "Одесса,Пушкинская,145/4", "dweed23", "Печенье", 30,
+                4);
+        System.out.println(product1.toString());
+    }
+}
